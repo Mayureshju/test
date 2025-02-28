be705d2f05ed559133e032657bf6bd0249aab01b
dhruvi204 <dhruvishah204@gmail.com>
2025-02-28 11:03:52 +0530

    feat(frappe-core): Modified frappe code (later to be overriden through service-further) for enabling floating input-label in data fields. <bugs> Not working for fields that inherit the data field

--- service_further/public/js/frappe/ui/form/base_input.js
+++ service_further/public/js/frappe/ui/form/base_input.js
@@ -1,19 +1,34 @@
 frappe.ui.form.ControlInput.prototype.set_input_areas = function () {
     if (this.only_input) {
-        this.input_area = this.wrapper;
+      this.input_area = this.wrapper;
     } else {
-        this.label_area = this.label_span = this.$wrapper.find("label").get(0);
-
-        console.log("Overridden!", this);
-        // hiding labels here
-        
-
-        // temporary thing to check
-
-        this.input_area = this.$wrapper.find(".control-input").get(0);
-        this.$input_wrapper = this.$wrapper.find(".control-input-wrapper");
-        // keep a separate display area to rendered formatted values
-        // like links, currencies, HTMLs etc.
-        this.disp_area = this.$wrapper.find(".control-value").get(0);
+      this.label_area = this.label_span = this.$wrapper.find("label").get(0);
+      console.log("Overridden!", this);
+      
+      // Hide the label
+      this.label_area.style.display = "none";
+      
+      // Get the label text
+      const labelText = this.label_area.textContent || this.df?.label || "";
+      console.log("Label text:", labelText);
+      
+      // Find the input field and set placeholder
+      const $input = this.$wrapper.find('input, textarea, select');
+      if ($input.length) {
+        $input.attr('placeholder', labelText);
+      } else {
+        setTimeout(() => {
+          const $delayedInput = this.$wrapper.find('input, textarea, select');
+          if ($delayedInput.length) {
+            console.log("Input found",this);
+            $delayedInput.attr('placeholder', labelText);
+          }
+        }, 100);
+      }
+      
+      this.input_area = this.$wrapper.find(".control-input").get(0);
+      this.$input_wrapper = this.$wrapper.find(".control-input-wrapper");
+      this.disp_area = this.$wrapper.find(".control-value").get(0);
     }
-}
+  }
+
