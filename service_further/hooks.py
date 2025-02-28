a4647ccbbcf9f8371c60d58b71664abe35ecd19d
dhruvi204 <dhruvishah204@gmail.com>
2025-02-28 14:42:34 +0530

    previous commit files

--- service_further/hooks.py
+++ service_further/hooks.py
@@ -25,7 +25,7 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = ["desk_override.bundle.scss"]
+app_include_css = ["desk_override.bundle.css"]
 app_include_js = ["service_further.bundle.js"]
 
 # include js, css files in header of web template
--- service_further/public/js/frappe/ui/form/base_input.js
+++ service_further/public/js/frappe/ui/form/base_input.js
@@ -1,34 +1,35 @@
-frappe.ui.form.ControlInput.prototype.set_input_areas = function () {
-    if (this.only_input) {
-      this.input_area = this.wrapper;
-    } else {
-      this.label_area = this.label_span = this.$wrapper.find("label").get(0);
-      console.log("Overridden!", this);
+// frappe.ui.form.ControlInput.prototype.set_input_areas = function () {
+//     if (this.only_input) {
+//       this.input_area = this.wrapper;
+//     } else {
+//       this.label_area = this.label_span = this.$wrapper.find("label").get(0);
+//       console.log("Overridden!", this);
       
-      // Hide the label
-      this.label_area.style.display = "none";
+//       // Hide the label
+//       this.label_area.style.display = "none";
       
-      // Get the label text
-      const labelText = this.label_area.textContent || this.df?.label || "";
-      console.log("Label text:", labelText);
+//       // Get the label text
+//       const labelText = this.label_area.textContent || this.df?.label || "";
+//       console.log("Label text:", labelText);
       
-      // Find the input field and set placeholder
-      const $input = this.$wrapper.find('input, textarea, select');
-      if ($input.length) {
-        $input.attr('placeholder', labelText);
-      } else {
-        setTimeout(() => {
-          const $delayedInput = this.$wrapper.find('input, textarea, select');
-          if ($delayedInput.length) {
-            console.log("Input found",this);
-            $delayedInput.attr('placeholder', labelText);
-          }
-        }, 100);
-      }
+//       // Find the input field and set placeholder
+//       const $input = this.$wrapper.find('input, textarea, select');
+//       if ($input.length) {
+//         $input.attr('placeholder', labelText);
+//       } else {
+//         setTimeout(() => {
+//           const $delayedInput = this.$wrapper.find('input, textarea, select');
+//           if ($delayedInput.length) {
+//             console.log("Input found",this);
+//             $delayedInput.attr('placeholder', labelText);
+//           }
+//         }, 100);
+//       }
       
-      this.input_area = this.$wrapper.find(".control-input").get(0);
-      this.$input_wrapper = this.$wrapper.find(".control-input-wrapper");
-      this.disp_area = this.$wrapper.find(".control-value").get(0);
-    }
-  }
+//       this.input_area = this.$wrapper.find(".control-input").get(0);
+//       this.$input_wrapper = this.$wrapper.find(".control-input-wrapper");
+//       this.disp_area = this.$wrapper.find(".control-value").get(0);
+//     }
+//   }
+
 
